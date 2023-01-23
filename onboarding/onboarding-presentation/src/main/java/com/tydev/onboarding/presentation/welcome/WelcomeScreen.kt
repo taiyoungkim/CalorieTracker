0e86608075e65006bf3e726f7140b36e62ac6126
taeyoungkim <ktyoung153@gmail.com>
2023-01-23 19:10:19 +0900

    fix:dekekt,ktlint 적용

--- onboarding/onboarding-presentation/src/main/java/com/tydev/onboarding/presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding-presentation/src/main/java/com/tydev/onboarding/presentation/welcome/WelcomeScreen.kt
@@ -14,8 +14,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.tydev.core.ui.LocalSpacing
 import com.tydev.core.R
+import com.tydev.core.ui.LocalSpacing
 import com.tydev.onboarding.presentation.components.ActionButton
 
 @Composable
