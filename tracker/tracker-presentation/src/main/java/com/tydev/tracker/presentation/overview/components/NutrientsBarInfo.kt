d1273a8e3a905b4e0df97a5b8796798d34806a89
taeyoungkim <ktyoung153@gmail.com>
2023-01-28 20:18:52 +0900

    fix:NutrientsBarInfo 상단 padding 추가

--- tracker/tracker-presentation/src/main/java/com/tydev/tracker/presentation/overview/components/NutrientsBarInfo.kt
+++ tracker/tracker-presentation/src/main/java/com/tydev/tracker/presentation/overview/components/NutrientsBarInfo.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -71,7 +72,7 @@ fun NutrientsBarInfo(
             )
         }
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize().padding(top = 10.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             UnitDisplay(
