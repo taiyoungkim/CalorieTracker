d17ec45916ccc5bd5938bf6268f96c98acd470e2
taeyoungkim <ktyoung153@gmail.com>
2023-02-21 21:00:53 +0900

    fix:Welcome페이지 뷰 수정

--- onboarding/onboarding-presentation/src/main/java/com/tydev/onboarding/presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding-presentation/src/main/java/com/tydev/onboarding/presentation/welcome/WelcomeScreen.kt
@@ -1,9 +1,12 @@
 package com.tydev.onboarding.presentation.welcome
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -11,6 +14,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import com.tydev.core.R
@@ -25,10 +31,17 @@ fun WelcomeScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(spacing.spaceMedium),
-        verticalArrangement = Arrangement.Center,
+            .padding(spacing.spaceLarge),
+        verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        // TODO:다른 gif로 수정하기
+        Image(
+            painter = painterResource(id = R.drawable.ic_burger),
+            modifier = Modifier.fillMaxWidth(0.8f),
+            contentScale = ContentScale.FillWidth,
+            contentDescription = null
+        )
         Text(
             text = stringResource(id = R.string.welcome_text),
             textAlign = TextAlign.Center,
@@ -40,7 +53,7 @@ fun WelcomeScreen(
             // 보통 presentation 로직을 viewModel 을 통해 구현해야하지만
             // 해당 기능만 존재하는 화면이므로 뷰모델 없이 구현(오버엔지니어링이라 생각)
             onClick = { onNextClick() },
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+            modifier = Modifier.fillMaxWidth()
         )
     }
 }
