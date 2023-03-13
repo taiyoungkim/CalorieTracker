e976c734e44ccd5adbbfb7d7e49d09e936aa2fd9
taeyoungkim <ktyoung153@gmail.com>
2023-03-13 19:17:44 +0900

    feature:tracker Domain UseCase TestCode 작성
    SearchFoodUseCase
    UpdateTrackeredFoodUseCase

---
+++ tracker/tracker-domain/src/test/java/com/tydev/tracker/domain/usecase/SearchFoodUseCaseTest.kt
@@ -0,0 +1,70 @@
+package com.tydev.tracker.domain.usecase
+
+import com.tydev.tracker.domain.model.TrackableFood
+import com.tydev.tracker.domain.repository.TrackerRepository
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class SearchFoodUseCaseTest {
+
+    @MockK
+    private lateinit var trackerRepository: TrackerRepository
+    private lateinit var searchFoodUseCase: SearchFoodUseCase
+
+    @Before
+    fun before() {
+        trackerRepository = mockk(relaxed = true)
+        searchFoodUseCase = SearchFoodUseCase(trackerRepository)
+    }
+
+    @Test
+    fun `given a blank query, return empty list`() = runTest {
+        // Given
+        val query = ""
+
+        // When
+        val result = searchFoodUseCase(query)
+
+        // Then
+        assertTrue(result.isSuccess)
+        assertEquals(emptyList<TrackableFood>(), result.getOrNull())
+    }
+
+    @Test
+    fun `given a non-blank query, return search results`() = runTest {
+        // Given
+        val query = "chicken"
+        val expectedFoods = listOf(
+            TrackableFood(
+                name = "Grilled chicken breast",
+                carbsPer100g = 0,
+                proteinPer100g = 31,
+                fatPer100g = 3,
+                caloriesPer100g = 165,
+                imageUrl = "https://example.com/chicken.jpg"
+            ),
+            TrackableFood(
+                name = "Chicken Caesar salad",
+                carbsPer100g = 4,
+                proteinPer100g = 17,
+                fatPer100g = 14,
+                caloriesPer100g = 233,
+                imageUrl = "https://example.com/caesar.jpg"
+            )
+        )
+        coEvery { trackerRepository.searchFood(query, DEFAULT_PAGE, DEFAULT_PAGE_SIZE) } returns Result.success(expectedFoods)
+
+        // When
+        val result = searchFoodUseCase(query)
+
+        // Then
+        assertTrue(result.isSuccess)
+        assertEquals(expectedFoods, result.getOrNull())
+    }
+}
---
+++ tracker/tracker-domain/src/test/java/com/tydev/tracker/domain/usecase/UpdateTrackedFoodUseCaseTest.kt
@@ -0,0 +1,65 @@
+package com.tydev.tracker.domain.usecase
+
+import com.tydev.tracker.domain.model.MealType
+import com.tydev.tracker.domain.model.TrackedFood
+import com.tydev.tracker.domain.repository.TrackerRepository
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import java.time.LocalDate
+import kotlin.math.roundToInt
+
+class UpdateTrackedFoodUseCaseTest {
+
+    @MockK
+    private lateinit var trackerRepository: TrackerRepository
+    private lateinit var updateTrackedFoodUseCase: UpdateTrackedFoodUseCase
+
+    @Before
+    fun before() {
+        trackerRepository = mockk(relaxed = true)
+        updateTrackedFoodUseCase = UpdateTrackedFoodUseCase(trackerRepository)
+    }
+
+    @Test
+    fun `update tracked food should call repository updateTrackedFood`() = runTest {
+        val trackedFood = TrackedFood(
+            id = 1,
+            name = "Apple",
+            carbs = 15,
+            protein = 0,
+            fat = 0,
+            calories = 60,
+            imageUrl = "apple_image_url",
+            mealType = MealType.Breakfast,
+            amount = 1,
+            date = LocalDate.now(),
+            carbsPerGram = 0.15f,
+            proteinPerGram = 0f,
+            fatPerGram = 0f,
+            caloriePerGram = 0.6f
+        )
+
+        val updatedAmount = 2
+
+        coEvery { trackerRepository.updateTrackedFood(any()) } just Runs
+
+        updateTrackedFoodUseCase(trackedFood, updatedAmount)
+
+        val updatedTrackedFood = trackedFood.copy(
+            carbs = (trackedFood.carbsPerGram * updatedAmount).roundToInt(),
+            protein = (trackedFood.proteinPerGram * updatedAmount).roundToInt(),
+            fat = (trackedFood.fatPerGram * updatedAmount).roundToInt(),
+            calories = (trackedFood.caloriePerGram * updatedAmount).roundToInt(),
+            amount = updatedAmount
+        )
+
+        coVerify { trackerRepository.updateTrackedFood(updatedTrackedFood) }
+    }
+}
